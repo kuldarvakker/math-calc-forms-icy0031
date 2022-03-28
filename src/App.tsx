49d461c13f8c157843dffba3c65c3a7207d8261b
kuldarvakker <kuvakk@taltech.ee>
2022-03-28 22:39:23 +0300

    Forumals and Cards Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import {Container, Col, Row, Form} from "react-bootstrap";
 import FormCard from "./components/FormCard";
-import { AjaldatudKasumiVaartus } from "./formulas/AjaldatudKasumiVaartus";
+import {AjaldatudKasumiVaartus, Roi, Tulumaar, RoiYearly, RoaRoe, TasuvusAeg} from "./formulas/AjaldatudKasumiVaartus";
 
 function App() {
 
     const [state, setState] = useState([NaN, "No answer"]);
+    const [state1, setState1] = useState([NaN, "No answer"]);
+    const [roi, setRoi] = useState([NaN, "No answer"]);
+    const [roiYearly, setRoiYearly] = useState([NaN, "No answer"]);
+    const [roa, setRoa] = useState([NaN, "No answer"]);
+    const [roe, setRoe] = useState([NaN, "No answer"]);
+    const [tasuvusAeg, setTasuvusAeg] = useState([NaN, "No answer"]);
+
 
 
     return (
@@ -28,6 +35,89 @@ function App() {
                                 ]}
                             answer={state}
                         />
+
+                        <FormCard
+                            title={'Investori tulumäär'}
+                            handleSubmit={(e) => setState1(Tulumaar(e))}
+                            text={[
+                                ' Investor ostis börsilt aktsiaid ',
+                                <Form.Control type="text" style={{display: "inline", width: "7rem"}} placeholder="5" />,
+                                ' € ulatuses ja müüs nad maha ',
+                                <Form.Control type="text" style={{display: "inline", width: "5rem"}} placeholder="2" />,
+                                ' päeva pärast teenides nende eest pärast teenustasude maha arvamist ',
+                                <Form.Control type="text" style={{display: "inline", width: "7rem"}} placeholder="4" />,
+                                ' €. Kui suur oli selle investori tulumäär?'
+                            ]}
+                            answer={state1}
+                        />
+
+                        <FormCard
+                            title={'ROI'}
+                            handleSubmit={(e) => setRoi(Roi(e))}
+                            text={[
+                                'Investor ostis kinnisvara ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="500000" />,
+                                ' € eest ja müüs selle kahe aasta pärast maha ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="2200000" />,
+                                ' € eest.'
+                            ]}
+                            answer={roi}
+                        />
+
+                        <FormCard
+                            title={'ROI yearly'}
+                            handleSubmit={(e) => setRoiYearly(RoiYearly(e))}
+                            text={[
+                                'Investor ostis kinnisvara ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="500000" />,
+                                ' € eest ja müüs selle ',
+                                <Form.Control type="text" style={{display: "inline", width: "5rem"}} placeholder="453" />,
+                                ' päeva pärast maha ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="2200000" />,
+                                ' € eest.'
+                            ]}
+                            answer={roiYearly}
+                        />
+
+                        <FormCard
+                            title={'ROA'}
+                            handleSubmit={(e) => setRoa(RoaRoe(e))}
+                            text={[
+                                'Ettevõtte aastane puhaskasum oli ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="500000" />,
+                                ' €. Aktivate väärtus ettevõtte bilansi kohaselt olid ',
+                                <Form.Control type="text" style={{display: "inline", width: "5rem"}} placeholder="453" />,
+                                ' €. Kui suur on ettevõtte ROA.'
+                            ]}
+                            answer={roa}
+                        />
+
+                        <FormCard
+                            title={'ROE'}
+                            handleSubmit={(e) => setRoe(RoaRoe(e))}
+                            text={[
+                                'Ettevõtte aastane puhaskasum oli ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="500000" />,
+                                ' €. Aktsionäride omakapital on ettevõtte bilansi kohaselt olid ',
+                                <Form.Control type="text" style={{display: "inline", width: "5rem"}} placeholder="453" />,
+                                ' €. Kui suur on ettevõtte ROE.'
+                            ]}
+                            answer={roe}
+                        />
+
+                        <FormCard
+                            title={'Tasuvusaeg'}
+                            handleSubmit={(e) => setTasuvusAeg(TasuvusAeg(e))}
+                            text={[
+                                'Ettevõtte kavatseb teha uude tehnoloogiasse investeeringut ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="500000" />,
+                                ' €. Prognooside kohaselt peaks see tagama iga-aastase sissetuleva rahavoo ',
+                                <Form.Control type="text" style={{display: "inline", width: "10rem"}} placeholder="453" />,
+                                ' €. Mitme aastaga tasub ära mainitud investeering?'
+                            ]}
+                            answer={tasuvusAeg}
+                        />
+
                     </Col>
                     <Col/>
                 </Row>
--- src/formulas/AjaldatudKasumiVaartus.tsx
+++ src/formulas/AjaldatudKasumiVaartus.tsx
@@ -22,3 +22,110 @@ export const AjaldatudKasumiVaartus = (event: FormEvent<HTMLFormElement>) => {
     }
     return [result, resultString]
 }
+
+export const Tulumaar = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1] / 365;
+    let c = numbers[2];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = Math.pow(c / a, 1 / b) - 1;
+    resultString = "ehk " + (result * 100) + "%";
+    return [result, resultString]
+}
+
+
+export const Roi = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = (b - a) / a;
+    resultString = "ehk " + (result * 100) + "%";
+    return [result, resultString]
+}
+
+
+export const RoiYearly = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1];
+    let c = numbers[2];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = ((c - a) / a) / (b / 365);
+    resultString = "ehk " + (result * 100) + "%";
+    return [result, resultString]
+}
+
+export const RoaRoe = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = a / b;
+    resultString = "ehk " + (result * 100) + "%";
+    return [result, resultString]
+}
+
+export const TasuvusAeg = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = a / b;
+    resultString = "aastaga";
+    return [result, resultString]
+}
+
+
+export const TasuvusPunkt = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let unCheckedFloats = [...Array.from(event.target as HTMLFormElement)
+        .map(i => parseFloat((i as HTMLInputElement).value))];
+    let numbers = [...unCheckedFloats.filter(elem => !isNaN(elem))];
+
+    let a = numbers[0];
+    let b = numbers[1];
+    let c = numbers[2];
+
+    let result : number = 0;
+    let resultString : string = "";
+    result = b / (1 - (a/c));
+    return [result, resultString]
+}
