f14bf6ab36c411fee4cebefa38838640fc5b9a2e
kuldarvakker <kuvakk@taltech.ee>
2022-03-28 16:29:27 +0300

    initial, ehh

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FormEvent} from 'react';
 import './App.css';
+import {Card, Button, Form, Container, Col, Row} from "react-bootstrap";
+
+// type Props = {
+//     handleSubmit: (event: FormEvent<HTMLElement>) => void,
+// };
+
+const Form1 = () => {
+
+
+    // const handleSubmit = props.handleSubmit as Props.handleSubmit;
+
+    const handleSubmit = (event : FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        Array.from(event.target as HTMLFormElement).forEach((input) => {
+            console.log(input);
+        });
+    }
+
+    return(
+        <Card>
+            <Card.Body>
+                <Form onSubmit={(event) => handleSubmit(event)}>
+                    <Card.Title>Ajaldatud kasumi väärtus</Card.Title>
+                    <Card.Text>
+                        Investor soovib osta N ettevõtte aktsiaid, mis ei ole börsiettevõte.
+                        Ettevõtte omanikud on ajakirjanduse kaudu teatanud,
+                        et nemad maksavad vähemalt <input type="text" placeholder="5" /> aastat dividende, suuruses <input type="text" placeholder="2"/> € aktsia kohta.
+                        Pikaajaliseks tulumääraks antud riigis prognoositakse <input type="text" placeholder="4" />% aastas.
+                        Millise hinnaga oleks mõistlik aktsiat osta?
+                    </Card.Text>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    );
+}
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Container style={{"marginTop": "1.5rem"}}>
+              <Row>
+                  <Col />
+                  <Col lg={6}>
+                      <Form1
+                          // handleSubmit={(event : FormEvent<HTMLFormElement>) => {
+                          // alert("aaa");
+                          // }}
+
+                      />
+                  </Col>
+                  <Col />
+              </Row>
+          </Container>
+      </div>
   );
 }
 
